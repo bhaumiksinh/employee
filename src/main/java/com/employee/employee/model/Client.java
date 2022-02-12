ad81167d89628f666c5d9b1cf0bef0ca3716de0c
bhaumiksinh <bhaumiksinh@hotmail.com>
2022-02-12 19:24:03 +0530

    2nd commit

---
+++ src/main/java/com/employee/employee/model/Client.java
@@ -0,0 +1,53 @@
+package com.employee.employee.model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id ; 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Employee> getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(List<Employee> employee) {
+        this.employee = employee;
+    }
+
+    private String name;
+    private String email;
+    
+    @ManyToMany
+    private List<Employee> employee;
+}
